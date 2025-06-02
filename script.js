adac81b1d7d503c66531cacbe4f4e3c9e06bb8c7
datacorp25 <contatodeyvidi@hotmail.com>
2025-06-02 17:33:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById("app").innerHTML = `
+  <div class='max-w-md mx-auto mt-10 bg-white p-6 rounded shadow'>
+    <h1 class='text-xl font-bold mb-4 text-center'>FLUXODRIVER</h1>
+    <p class='text-center text-gray-500'>Versão completa com arquivos separados.</p>
+    <!-- Aqui entrariam as funcionalidades JS reais (login, registros, resumo, etc) -->
+  </div>
+`;
